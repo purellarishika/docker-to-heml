7863005a797d0ed316a1f0f45e36a2f1b8536247
unknown <purellarishika@gmail.com>
2025-03-30 02:01:36 +0530

    docker migration to heml

--- docker-migration-to-heml-main/my-docker-image/index.js
+++ docker-migration-to-heml-main/my-docker-image/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.get('/hello', (req, res) => {
   res.send('Hello from Docker in VS Code!');
